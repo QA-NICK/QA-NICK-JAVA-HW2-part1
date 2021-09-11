cec6481f320adf95975f9a20cc9cda31f1cd5374
Nick <zxmadjik@mail.ru>
2021-09-11 16:42:59 +0400

    insert src

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 516;
+        int bonusCost = 20;
+        int totalBonus = ticketPrice / bonusCost;
+        System.out.println(totalBonus);
+    }
+}
